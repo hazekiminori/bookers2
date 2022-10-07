02251534ae805bc12507d1cf79ba9ff1e6ad16fc
EC2 Default User <ec2-user@ip-172-31-30-106.ap-northeast-1.compute.internal>
2022-10-07 08:18:56 +0000

    sign upページへ移行できた

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,35 @@
 class BooksController < ApplicationController
   def show
+    @book = Book.find(params[:id])
   end
 
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def new
     @book = Book.new
   end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+       redirect_to book_path
+    else 
+      @book = Book.all
+      render :index
+    end
+  end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to book_path
+  end 
+  
 end
