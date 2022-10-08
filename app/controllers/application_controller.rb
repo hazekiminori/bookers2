09cfe82730bf4286c299b52d6cdc2358ad234479
EC2 Default User <ec2-user@ip-172-31-30-106.ap-northeast-1.compute.internal>
2022-10-08 14:51:03 +0000

    プロフ編集中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   
+  before_action :authenticate_user!, expect:[:top]
+
   def after_sign_in_path_for(resource)
-    user_path(id: current_user)
+    about_path
   end
   
   def after_sign_out_path_for(resouece)
