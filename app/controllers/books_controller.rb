e8e96d9209a16cc1007f9be2ad0ee9e9453a4396
EC2 Default User <ec2-user@ip-172-31-30-106.ap-northeast-1.compute.internal>
2022-10-20 18:18:56 +0000

    エラー残９個

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,9 @@
 class BooksController < ApplicationController
-  
+
   before_action :current_user, only: [:edit, :update, :destroy]
   before_action :authenticate_user!
-  
+
   def show
-    @user = User.find(params[:id])
     @book = Book.find(params[:id])
     @books = @book.user.books
     @book_new = Book.new
@@ -19,41 +18,41 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     @book = Book.find(params[:id])
     @book.user_id = current_user.id
-    
+
     if @book.update(book_params)
-       redirect_to book_path(@book) 
+       redirect_to book_path(@book)
        flash[:notice]="You have updated book successfully."
     else
        render :edit
     end
   end
-  
+
   def create
     @book=Book.new(book_params)
     @book.user_id=current_user.id
     if @book.save
        redirect_to book_path(@book.id)
        flash[:notice] = "You have created book successfully."
-    else 
+    else
       render :index
     end
   end
-  
+
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path
-  end 
-  
+  end
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
-  
+
+
 end
